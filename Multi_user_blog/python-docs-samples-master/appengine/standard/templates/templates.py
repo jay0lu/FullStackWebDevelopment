b8f2bd173915a0cd3ccd0ae395b3d4c29497d1bf
jay0lu <luzheng1992@gmail.com>
2017-01-19 11:50:19 -0500

    prepare for shopping list function

--- Multi_user_blog/python-docs-samples-master/appengine/standard/templates/templates.py
+++ Multi_user_blog/python-docs-samples-master/appengine/standard/templates/templates.py
@@ -25,24 +25,6 @@ template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir))
 
 
-hidden_html = """
-    <input type="hidden" name="food" value="%s">
-"""
-
-item_html = """
-    <li>%s</li>
-"""
-
-shopping_list_html = """
-    <br>
-    <br>
-    <h2>Shopping List</h2>
-    <ul>
-        %s
-    </ul>
-"""
-
-
 class Handler(webapp2.RequestHandler):
     """docstring for handler. handler"""
     def write(self, *a, **kw):
@@ -58,24 +40,8 @@ class Handler(webapp2.RequestHandler):
 
 class MainPage(Handler):
     def get(self):
-        self.render("shopping_list.html")
-
-        # output = form_html
-        # output_hidden = ""
-        #
-        # items = self.request.get_all("food")
-        # if items:
-        #     output_items = ""
-        #     for item in items:
-        #         output_hidden += hidden_html % item
-        #         output_items += item_html % item
-        #
-        #     output_shopping = shopping_list_html % output_items
-        #     output += output_shopping
-        #
-        # output = output % output_hidden
-        #
-        # self.write(output)
+        items = self.request.get_all("food")
+        self.render("shopping_list.html", items=items)
 
 
 class FizzBuzzHandler(Handler):
